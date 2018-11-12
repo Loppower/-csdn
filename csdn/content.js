c96b986f48b17f2fd289e2f332fae5b54eecbd35
loppower <935166174@qq.com>
2018-11-12 11:56:50 +0800

    first commit

---
+++ csdn/content.js
@@ -0,0 +1,28 @@
+
+setTimeout(function(){
+  if (window.location.host.search('csdn') !== -1) {
+    if(confirm("纯净版csdn?")){
+      console.log("csdn去广告")
+      var aside = document.getElementsByTagName('aside');
+      var recommendRight = document.getElementsByClassName('recommend-right');
+      var toolBox = document.getElementsByClassName('tool-box');
+      var meauGotopBox = document.getElementsByClassName('meau-gotop-box');
+      var recommendBox = document.getElementsByClassName('recommend-box');
+      var pulllogBox = document.getElementsByClassName('pulllog-box');
+      var mainBox = document.getElementById('mainBox');
+      var main = document.getElementsByTagName('main');
+
+      remove(aside);
+      remove(recommendRight);
+      remove(toolBox);
+      remove(meauGotopBox);
+      remove(recommendBox);
+      mainBox.append(document.getElementsByClassName('blog-content-box')[0]);
+      remove(main);
+      remove(pulllogBox);
+    }
+  }
+}, 1000);
+function remove(obj){
+  obj[0].parentNode.removeChild(obj[0]);
+}
