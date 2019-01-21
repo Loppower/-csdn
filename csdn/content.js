ae8cea0b2edd579b33ae33ff332ca8f1a1a1935b
ysh <935166174@qq.com>
2019-01-21 23:06:39 +0800

    修复某些页面中没有阅读更多按钮时无法执行脚本的问题

--- csdn/content.js
+++ csdn/content.js
@@ -2,8 +2,10 @@
 setTimeout(function(){
   if (window.location.host.search('csdn') !== -1) {
     if(confirm("纯净版csdn?")){
-      console.log("csdn去广告")
-      document.getElementById('btn-readmore').click();//先点击阅读更多的按钮，把隐藏的内容显示出来
+      console.log("csdn去广告");
+      if(document.getElementById('btn-readmore')){
+        document.getElementById('btn-readmore').click();//先点击阅读更多的按钮，把隐藏的内容显示出来
+      }
       var aside = document.getElementsByTagName('aside');
       var recommendRight = document.getElementsByClassName('recommend-right');
       var toolBox = document.getElementsByClassName('tool-box');
